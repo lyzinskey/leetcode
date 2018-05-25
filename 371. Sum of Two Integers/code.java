6c10b5210cf0218f7b38826c47711997231d6f19
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-05-25 11:19:19 -0700

    Create code.java

---
+++ 371. Sum of Two Integers/code.java
@@ -0,0 +1,8 @@
+//Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+//Example:
+//Given a = 1 and b = 2, return 3.
+
+    public int getSum(int a, int b) {
+        return b==0? a:getSum(a^b, (a&b)<<1); //be careful about the terminating condition;
+    }
