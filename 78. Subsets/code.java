20d66ab2a8e2286d34767734c63c2ad4e42e08c8
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-16 19:46:52 -0700

    Update code.java

--- 78. Subsets/code.java
+++ 78. Subsets/code.java
@@ -31,7 +31,7 @@
             return result;
         }
         
-        Arrays.sort(nums);
+//         Arrays.sort(nums);
         helper(new ArrayList<Integer>(), nums, 0, result);
         return result;
     }
@@ -58,6 +58,8 @@
     }
     
     
+
+
 //非递归：使用宽度优先搜索算法的做法（BFS）
 //一层一层的找到所有的子集：
 //
@@ -73,7 +75,7 @@
             return results; // 空列表
         }
         
-        Arrays.sort(nums);
+//         Arrays.sort(nums);
         
         // BFS
         Queue<List<Integer>> queue = new LinkedList<>();
@@ -93,5 +95,43 @@
         }
         
         return results;
+    }
+
+
+
+
+
+//使用组合类搜索的专用深度优先搜索算法。
+//一层一层的决策每个数要不要放到最后的集合里。
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();        
+      
+//         Arrays.sort(nums);
+        dfs(nums, 0, new ArrayList<Integer>(), results);
+        return results;
+    }
+    
+    // 1. 递归的定义
+    // 以 subset 开头的，配上 nums 以 index 开始的数所有组合放到 results 里
+    private void dfs(int[] nums,
+                     int index,
+                     List<Integer> subset,
+                     List<List<Integer>> results) {
+        // 3. 递归的出口
+        if (index == nums.length) {
+            results.add(new ArrayList<Integer>(subset));
+            return;
+        }
+        
+        // 2. 递归的拆解
+        // (如何进入下一层)
+        
+        // 选了 nums[index]
+        subset.add(nums[index]);
+        dfs(nums, index + 1, subset, results);
+        
+        // 不选 nums[index]
+        subset.remove(subset.size() - 1);
+        dfs(nums, index + 1, subset, results);
     }
 
