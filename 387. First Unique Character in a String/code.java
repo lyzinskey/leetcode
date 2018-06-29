e3336c83f954674afe22b0089c9ab990dd0fa41f
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-28 21:15:11 -0700

    Create code.java

---
+++ 387. First Unique Character in a String/code.java
@@ -0,0 +1,32 @@
+//Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+//Examples:
+//
+//s = "leetcode"
+//return 0.
+//
+//s = "loveleetcode",
+//return 2.
+//Note: You may assume the string contain only lowercase letters.
+
+
+
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                if (map.get(s.charAt(i)) != null) {
+                    map.remove(s.charAt(i));
+                }
+            } else {
+                map.put(s.charAt(i), i);
+                set.add(s.charAt(i));
+            }
+        }
+        return map.size() == 0 ? -1 : map.entrySet().iterator().next().getValue();
+    }
+}
+
+
