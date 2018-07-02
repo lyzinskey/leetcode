6f616e24be5bceb587999254800939b9c0ed6938
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-07-02 10:09:53 -0700

    Update code.java

--- 658. Find K Closest Elements/code.java
+++ 658. Find K Closest Elements/code.java
@@ -16,6 +16,14 @@
 //Absolute value of elements in the array and x will not exceed 10^4
 
 
+
+//使用 Heapify 的方法
+//
+//heapify 时间复杂度 O(n)
+//然后 pop k 个点出来，时间复杂度 klogn
+//总共的时间复杂度 O(n + klogn)
+//如果 n >> k 的话，这种方法的时间复杂度是很优秀的。
+//
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = findLowerClosest(arr, x);
         int right = left + 1;
@@ -77,3 +85,49 @@
     }
     
     
+
+
+
+//基于 PriorityQueue 的方法
+//PriorityQueue 里从远到近排序。当 PQ 里超过 k 个的时候，就 pop 掉一个。
+//时间复杂度 O(nlogk + klogk)
+//
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (arr == null || arr.length == 0) {
+            return result;
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int diff = distance(o2, x) - distance(o1, x);
+                if (diff == 0) {
+                    diff = o2 - o1;
+                }
+                return diff;
+            }
+        });
+
+        for (int i = 0; i < arr.length; i++) {            
+            maxHeap.add(arr[i]);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();                                    
+            }
+        }       
+        
+        while (!maxHeap.isEmpty()) {
+            result.add(maxHeap.poll());
+        }
+        
+        Collections.sort(result);
+        
+        return result;
+    }
+
+    private int distance(int a, int b) {
+        return Math.abs(a - b);
+    }
+
+
