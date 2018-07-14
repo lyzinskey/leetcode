b09166ab328e73e3ba91490c8a140be0731d5f11
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-07-14 16:01:49 -0700

    Create code.java

---
+++ 303. Range Sum Query - Immutable/code.java
@@ -0,0 +1,42 @@
+//Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+//Example:
+//Given nums = [-2, 0, 3, -5, 2, -1]
+
+//sumRange(0, 2) -> 1
+//sumRange(2, 5) -> -1
+//sumRange(0, 5) -> -3
+
+//Note:
+//You may assume that the array does not change.
+//There are many calls to sumRange function.
+
+
+
+class NumArray {
+
+    private int[] sum;
+    
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        int subSum = 0;
+        for (int i = 1; i < sum.length; i++) {
+            subSum += nums[i - 1];
+            sum[i] = subSum;
+        }        
+    }
+    
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];        
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+ 
+ 
+ 
