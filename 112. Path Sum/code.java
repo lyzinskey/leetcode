342334fa59e764ac8db5cc2e8a5fbc37ccbcae57
Inskey <lyzinskey@gmail.com>
2018-09-10 15:20:09 -0700

    Update code.java

--- 112. Path Sum/code.java
+++ 112. Path Sum/code.java
@@ -27,43 +27,25 @@
  * }
  */
 class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return pathSum(root, sum, 0);
+    }
     
-    private List<List<Integer>> result = new ArrayList<>();
-    
-    public boolean hasPathSum(TreeNode root, int target) {
+    private boolean pathSum(TreeNode root, int sum, int prefixSum) {
         if (root == null) {
             return false;
         }
         
-        ArrayList<Integer> path = new ArrayList<Integer>();
-        path.add(root.val);
-        helper(root, path, root.val, target);
-        return result.size() == 0 ? false : true;        
-    }
-    
-    private void helper(TreeNode root, ArrayList<Integer> path, int sum, int target) {
-                            
-        // meet leaf
         if (root.left == null && root.right == null) {
-            if (sum == target) {
-                result.add(new ArrayList<Integer>(path));
+            if (prefixSum + root.val == sum) {
+                return true;
             }
-            return;
+            return false;
         }
         
-        // go left
-        if (root.left != null) {
-            path.add(root.left.val);
-            helper(root.left, path, sum + root.left.val, target);
-            path.remove(path.size() - 1);
-        }
-        
-        // go right
-        if (root.right != null) {
-            path.add(root.right.val);
-            helper(root.right, path, sum + root.right.val, target);
-            path.remove(path.size() - 1);
-        }
-    }    
+        return pathSum(root.left, sum, prefixSum + root.val) || pathSum(root.right, sum, prefixSum + root.val);
+    }   
 }
+
+
 
