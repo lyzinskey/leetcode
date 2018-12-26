f7d5a8a3ad54dacfe18371daf9e6e17d4e55ae08
Inskey <lyzinskey@gmail.com>
2018-12-26 12:08:13 -0800

    Update code.java

--- 200. Number of Islands/code.java
+++ 200. Number of Islands/code.java
@@ -80,3 +80,44 @@ class Solution {
 
 
 
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {                    
+                    result++;
+                    DFS(grid, i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void DFS(char[][] grid, int i, int j) {
+        if (outOfBound(grid, i, j) || grid[i][j] == '0') {            
+            return;
+        }    
+        
+        grid[i][j] = '0';                    
+        DFS(grid, i + 1, j);
+        DFS(grid, i - 1, j);
+        DFS(grid, i, j + 1);
+        DFS(grid, i, j - 1);
+    }
+
+    private boolean outOfBound(char[][] grid, int x, int y) {
+        int row = grid.length;
+        int col = grid[0].length;
+        return x < 0 || x >= row || y < 0 || y >= col;
+    }
+}
+
+
+
