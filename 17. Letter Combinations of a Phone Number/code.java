0f3b7237ac475f38944f0b8a16d741efae94ca32
Inskey <lyzinskey@gmail.com>
2019-01-03 22:57:53 -0800

    Update code.java

--- 17. Letter Combinations of a Phone Number/code.java
+++ 17. Letter Combinations of a Phone Number/code.java
@@ -20,39 +20,34 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
         
-        if (digits == null || digits.length() == 0) {
+        if (digits.length() == 0) {
             return result;
         }
         
-        Map<Character, char[]> map = new HashMap<>();
-        map.put('0', new char[] {});
-        map.put('1', new char[] {});
-        map.put('2', new char[] { 'a', 'b', 'c' });
-        map.put('3', new char[] { 'd', 'e', 'f' });
-        map.put('4', new char[] { 'g', 'h', 'i' });
-        map.put('5', new char[] { 'j', 'k', 'l' });
-        map.put('6', new char[] { 'm', 'n', 'o' });
-        map.put('7', new char[] { 'p', 'q', 'r', 's' });
-        map.put('8', new char[] { 't', 'u', 'v'});
-        map.put('9', new char[] { 'w', 'x', 'y', 'z' });        
+        Map<Character, char[]> hashmap = new HashMap<>();
+        hashmap.put('2', new char[] {'a', 'b', 'c'});
+        hashmap.put('3', new char[] {'d', 'e', 'f'});
+        hashmap.put('4', new char[] {'g', 'h', 'i'});
+        hashmap.put('5', new char[] {'j', 'k', 'l'});
+        hashmap.put('6', new char[] {'m', 'n', 'o'});
+        hashmap.put('7', new char[] {'p', 'q', 'r', 's'});
+        hashmap.put('8', new char[] {'t', 'u', 'v'});
+        hashmap.put('9', new char[] {'w', 'x', 'y', 'z'});
         
-        StringBuilder sb = new StringBuilder();
-        
-        dfs(digits, result, map, sb);
-        
-        return result;
+        DFS(digits, result, hashmap, new StringBuilder(), 0);
+        return result;    
     }
     
-    private void dfs(String digits, List<String> result, Map<Character, char[]> map, StringBuilder sb) {
+    private void DFS(String digits, List<String> result, Map<Character, char[]> hashmap, StringBuilder sb, int index) {
         if (sb.length() == digits.length()) {
-            result.add(new String(sb));
+            result.add(sb.toString());
             return;
         }
         
-        for (char c : map.get(digits.charAt(sb.length()))) {
-            sb.append(c);
-            dfs(digits, result, map, sb);
-            sb.setLength(sb.length() - 1);
+        for (char ch : hashmap.get(digits.charAt(index))) {
+            sb.append(ch);
+            DFS(digits, result, hashmap, sb, index + 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
