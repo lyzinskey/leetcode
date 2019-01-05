d51020d0691235ebff44b913603fd43f6778731f
Inskey <lyzinskey@gmail.com>
2019-01-04 22:52:37 -0800

    Update code.java

--- 160. Intersection of Two Linked Lists/code.java
+++ 160. Intersection of Two Linked Lists/code.java
@@ -18,37 +18,56 @@
 //Your code should preferably run in O(n) time and use only O(1) memory.
 
 
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode(int x) {
-     *         val = x;
-     *         next = null;
-     *     }
-     * }
-     */
-     
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a = headA;
-        ListNode b = headB;
-        while (a != null || b != null) {
-            if (a != null) {
-                a = a.next;
-            } else {
-                headB = headB.next;
-            }
-            if (b != null) {
-                b = b.next;
-            } else {
+        int lenA = length(headA);
+        int lenB = length(headB);
+        int diff = Math.abs(lenA - lenB);
+        
+        if (lenA > lenB) {
+            while (diff > 0) {
                 headA = headA.next;
+                diff--;
+            }
+        } else {
+            while (diff > 0) {
+                headB = headB.next;
+                diff--;
             }
         }
+        
         while (headA != headB) {
             headA = headA.next;
             headB = headB.next;
-        }        
+        }
+        
         return headA;
     }
+    
+    private int length(ListNode head) {
+        int length = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            curr = curr.next;
+            length++;
+        }
+        return length;
+    }
+}
+
+
 
