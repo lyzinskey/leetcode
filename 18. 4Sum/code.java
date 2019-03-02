9598aece7e4ecc9e27f2d1121ecb68dfa13e608f
Inskey <lyzinskey@gmail.com>
2019-03-01 16:25:16 -0800

    Update code.java

--- 18. 4Sum/code.java
+++ 18. 4Sum/code.java
@@ -21,57 +21,45 @@
 
 class Solution {
     public List<List<Integer>> fourSum(int[] num, int target) {
-        ArrayList<List<Integer>> result = new ArrayList<>();
-        if (num.length < 4) {
-            return result;
-        }
-        Arrays.sort(num);
-        for (int i = 0; i < num.length - 3; i++) {
-            if (num[i] + num[i + 1] + num[i + 2] + num[i + 3] > target) {
-                break; //first candidate too large, search finished
-            }
-            if (num[i] + num[num.length - 1] + num[num.length - 2] + num[num.length - 3] < target) {
-                continue; //first candidate too small
-            }
-            if (i > 0 && num[i] == num[i - 1]) {
-                continue; //prevents duplicate result in result list
-            }
-            for (int j = i + 1; j < num.length - 2; j++) {
-                if (num[i] + num[j] + num[j + 1] + num[j + 2] > target) {
-                    break; //second candidate too large
-                }
-                if (num[i] + num[j] + num[num.length - 1] + num[num.length - 2] < target) {
-                    continue; //second candidate too small
-                }
-                if (j > i + 1 && num[j] == num[j - 1]) {
-                    continue; //prevents duplicate results in result list
-                }
-                int low = j + 1, high = num.length - 1;
-                while (low < high) {
-                    int sum = num[i] + num[j] + num[low] + num[high];
-                    if (sum == target) {
-                        result.add(Arrays.asList(num[i], num[j], num[low], num[high]));
-                        while (low < high && num[low] == num[low + 1]) {
-                            low++; //skipping over duplicate on low
-                        }
-                        while (low < high && num[high] == num[high - 1]) {
-                            high--; //skipping over duplicate on high
-                        }
-                        low++;
-                        high--;
-                    }
-                    //move window
-                    else if (sum < target) {
-                        low++;
-                    } else {
-                        high--;
-                    }
-                }
-            }
-        }
-        return result;
-    }
+		List<List<Integer>> rst = new ArrayList<List<Integer>>();
+		Arrays.sort(num);
+
+		for (int i = 0; i < num.length - 3; i++) {
+			if (i != 0 && num[i] == num[i - 1]) {
+				continue;
+			}
+
+			for (int j = i + 1; j < num.length - 2; j++) {
+				if (j != i + 1 && num[j] == num[j - 1]) {
+                    continue;
+                }					
+
+				int left = j + 1;
+				int right = num.length - 1;
+				while (left < right) {
+					int sum = num[i] + num[j] + num[left] + num[right];
+					if (sum == target) {
+					    rst.add(Arrays.asList(num[i], num[j], num[left], num[right]));
+						left++;
+						right--;
+						while (left < right && num[left] == num[left - 1]) {
+							left++;
+						}
+						while (left < right && num[right] == num[right + 1]) {
+							right--;
+						}
+					} else if (sum > target) {
+						right--;
+					} else {
+						left++;
+					}
+				}
+			}
+		}
+		return rst;
+	}
 }
+
 
 
 
