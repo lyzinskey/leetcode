1b196794d0d16cfa54deddf99c8b3a1f8090f3b2
Inskey <yanzhili@usc.edu>
2019-07-15 23:01:42 -0700

    Create code.java

---
+++ 977. Squares of a Sorted Array/code.java
@@ -0,0 +1,43 @@
+//Given an array of integers A sorted in non-decreasing order, 
+//return an array of the squares of each number, also in sorted non-decreasing order.
+
+//Example 1:
+//
+//Input: [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+
+//Example 2:
+//
+//Input: [-7,-3,2,3,11]
+//Output: [4,9,9,49,121]
+
+//Note:
+//
+//1 <= A.length <= 10000
+//-10000 <= A[i] <= 10000
+//A is sorted in non-decreasing order.
+
+
+
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int[] result = new int[A.length];
+        int i = 0;
+        int j = A.length - 1;
+        
+        for (int p = A.length - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
+
+
+
