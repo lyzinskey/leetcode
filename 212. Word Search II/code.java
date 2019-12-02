309936145d295aaecbf86e2b5b590dc30a7c8ddf
Inskey <lyzinskey@gmail.com>
2019-12-02 14:27:07 -0800

    Update code.java

--- 212. Word Search II/code.java
+++ 212. Word Search II/code.java
@@ -22,18 +22,15 @@
 
 
 
+
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> result = new ArrayList<>();
         if (board == null || board.length == 0) {
             return result;
         } 
-        
-        Arrays.sort(words);
+                
         for (int i = 0; i < words.length; i++) {
-            if (i > 0 && words[i].equals(words[i - 1])) {
-                continue;
-            }
             if (exist(board, words[i])) {
                 result.add(words[i]);
             }            
@@ -42,39 +39,36 @@ class Solution {
     }
     
     private boolean exist(char[][] board, String word) {
-        if (word == null || word.length() == 0) {
-            return true;
-        }
-        
+        boolean[][] visited = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (dfs(board, word, i, j, 0)) {
-                        return true;
-                    }
-                }
-                
+            for (int j = 0; j < board[0].length; j++) {                
+                if (dfs(board, word, i, j, 0, visited)) {
+                    return true;
+                }                                
             }
         }
         return false;
     }
     
-    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+    private boolean dfs(char[][] board, String word, int i, int j, int index, boolean[][] visited) {
         if (index == word.length()) {
             return true;
         }
         
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index)) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index) || visited[i][j]) {
             return false;
         }
         
-        board[i][j] = '?';
-        boolean result = dfs(board, word, i + 1, j, index + 1) ||
-                         dfs(board, word, i - 1, j, index + 1) ||
-                         dfs(board, word, i, j + 1, index + 1) ||
-                         dfs(board, word, i, j - 1, index + 1);
-        board[i][j] = word.charAt(index);
+        visited[i][j] = true;
+        boolean result = dfs(board, word, i + 1, j, index + 1, visited) ||
+                         dfs(board, word, i - 1, j, index + 1, visited) ||
+                         dfs(board, word, i, j + 1, index + 1, visited) ||
+                         dfs(board, word, i, j - 1, index + 1, visited);
+        visited[i][j] = false;
         return result;
     }    
 }
+
+
+
 
