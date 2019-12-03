81fe33f546c8bc7df7110f6dfe3f7d8776ca2869
Inskey <lyzinskey@gmail.com>
2019-12-02 20:33:55 -0800

    Update code.java

--- 81. Search in Rotated Sorted Array II/code.java
+++ 81. Search in Rotated Sorted Array II/code.java
@@ -19,6 +19,57 @@
 //Would this affect the run-time complexity? How and why?
 
 
+
+// Time: avg O(logn), worst case O(n)
+// Space: O(1)
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        
+        int left = 0;
+        int right = nums.length - 1;        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            /*
+            The only difference is that due to the existence of duplicates, 
+            we can have nums[left] == nums[mid] and in that case, 
+            the first half could be out of order 
+            (i.e. NOT in the ascending order, e.g. [3 1 2 3 3 3 3]) 
+            and we have to deal this case separately. 
+            In that case, it is guaranteed that nums[right] also equals to nums[mid], 
+            so what we can do is to check if nums[mid] == nums[left] == nums[right] 
+            before the original logic, 
+            and if so, we can move left and right both towards the middle by 1. and repeat.
+            */
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
 //    In-place, O(1) extra space and O(n) time.
     public boolean search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
@@ -67,3 +118,5 @@
         }
         return false;
     }
+
+
