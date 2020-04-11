7d64ae5435378ec5581a3b9fbe8ee1f6cfbb7649
Inskey <lyzinskey@gmail.com>
2020-04-10 21:24:38 -0700

    Update code.java

--- 791. Custom Sort String/code.java
+++ 791. Custom Sort String/code.java
@@ -26,15 +26,16 @@
 
 class Solution {
     // Time: O(S.length + T.length)
-    // Space: O(T.length)    
+    // Space: O(T.length)
     public String customSortString(String S, String T) {
         // count[char] = the number of occurrences of 'char' in T.
         // This is offset so that count[0] = occurrences of 'a', etc.
         // 'count' represents the current state of characters
         // (with multiplicity) we need to write to our answer.
         int[] count = new int[26];
-        for (char c: T.toCharArray())
+        for (char c: T.toCharArray()) {
             count[c - 'a']++;
+        }
 
         // ans will be our final answer.  We use StringBuilder to join
         // the answer so that we more efficiently calculate a
@@ -43,8 +44,9 @@ class Solution {
 
         // Write all characters that occur in S, in the order of S.
         for (char c: S.toCharArray()) {
-            for (int i = 0; i < count[c - 'a']; ++i)
+            for (int i = 0; i < count[c - 'a']; i++) {
                 ans.append(c);
+            }                
             // Setting count[char] to zero to denote that we do
             // not need to write 'char' into our answer anymore.
             count[c - 'a'] = 0;
@@ -52,9 +54,11 @@ class Solution {
 
         // Write all remaining characters that don't occur in S.
         // That information is specified by 'count'.
-        for (char c = 'a'; c <= 'z'; ++c)
-            for (int i = 0; i < count[c - 'a']; ++i)
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < count[c - 'a']; i++) {
                 ans.append(c);
+            }                
+        }
 
         return ans.toString();
     }
