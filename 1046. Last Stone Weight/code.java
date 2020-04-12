076883f6b24e05905028f0c5a5e85e2f27e9536f
Inskey <lyzinskey@gmail.com>
2020-04-12 15:35:37 -0700

    Update code.java

--- 1046. Last Stone Weight/code.java
+++ 1046. Last Stone Weight/code.java
@@ -29,11 +29,15 @@ class Solution {
     // Time: O(nlogn)
     // Space: O(n)
     public int lastStoneWeight(int[] A) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b - a);
-        for (int a : A)
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int a : A) {
             pq.offer(a);
-        while (pq.size() > 1)
-            pq.offer(pq.poll() - pq.poll());
+        }            
+        
+        while (pq.size() > 1) {
+            pq.offer(pq.poll() - pq.poll());            
+        }
         return pq.poll();
     }
 }
